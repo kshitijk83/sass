24ff877abf009f9bf448f2e2ca8e115f624935eb
kshitij kumar <kshitijk83@gmail.com>
2018-12-31 12:52:53 +0530

    smoth scrolling

--- js/index.js
+++ js/index.js
@@ -1,75 +1,29 @@
-$(document).ready(function(){
-    $('.navigation__item').click(function(){
-        $('.navigation__checkbox').prop('checked', false);
-        
-    })
-    $('#roomsuite').click(function(){
-        $('#room').prop('checked', true);
-    })
-    $('#cottagesuite').click(function(){
-        $('#cottage').prop('checked', true);
-    })
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPos = target.getBoundingClientRect().top;
+    var startPos = window.pageYOffset;
+    var distance = targetPos-startPos;
+    var startTime =null;
+  
 
-})
-
-function Submit(e) {
-    var name = document.getElementById("name").value.trim();
-    var email = document.getElementById("email").value.trim();
-    var contact = document.getElementById("contact").value.trim();
-    var startDate = document.getElementById("start-date").value.trim();
-    var endDate = document.getElementById("end-date").value.trim();
-    var people = document.getElementById("people").value.trim();
-    var roomType="";
-
-    if(document.getElementById("room").checked) {
-        roomType = document.getElementById("room").value;
-        document.getElementById("err").style.display="none";
-    }else if(document.getElementById("cottage").checked) {
-        roomType = document.getElementById("cottage").value;
-        document.getElementById("err").style.display="none";
+    function animation(currentTime){
+        if(!startTime) startTime = currentTime;
+        var timeElapsed = currentTime-startTime;
+        var run = ease(timeElapsed, startPos, distance, duration);
+        console.log(run);
+        window.scrollTo(0, run);
+        if(timeElapsed<duration) {requestAnimationFrame(animation);}
     }
 
-    if(!(name && email && contact && startDate && endDate && people)) {
-        return false;
-    }
-    if(!roomType) {
-        e.preventDefault();
-        $("#err").css('opacity', '1');
-        // $('.checkreq').text("Plz select one option in Checkbox");
-        // $('.blah')[0].click();
-        return false;
-    }
-
-    var objectToSend = {
-        "name": name,
-        "email": email,
-        "contact": contact,
-        "startDate": startDate,
-        "endDate": endDate,
-        "people": people,
-        "roomType": roomType
+    function ease(t, b, c, d) {
+        return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
     };
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "/adduser");
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xmlhttp.send(JSON.stringify(objectToSend));
-
-    xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState == 4){
-            var res = JSON.parse(xmlhttp.responseText);
-            console.log(res);
-            if(res==500){
-                console.log("rooms not empty");
-                document.getElementById("checkreq").innerHTML ="Rooms are not empty"
-                document.getElementById("blah").click();
-            }
-            else{
-                document.getElementById("myform").reset();
-                document.getElementById("checkreq").innerHTML ="Thank you, you will be contact shortly";
-                document.getElementById("blah").click();
-            }
-        }
-    }
-    e.preventDefault();
+    requestAnimationFrame(animation);
 }
+
+var learn = document.querySelector('.tour');
+
+learn.addEventListener('click', function(){
+    smoothScroll('.section-features', 1000);
+})
